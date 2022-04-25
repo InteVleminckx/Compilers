c2fb4d363a9f17d2d25ca0e5b6688e2a3f0ee890
Maksim Karnaukh <s0201604@ua.ad.ac.be>
2022-04-25 13:41:32 +0200

    files

--- project1/inputfile.c
+++ project1/inputfile.c
@@ -2,13 +2,17 @@
 
 int main() {
 
-	int x = 10;
-	int y = 11;
-	y = y + 1;
-	x = x + 1 / y + 1;
+
+    *5;
+//	int x = 10;
+//	int y = 11;
+//	y = y + 1;
+//	x = x + 1 * y + 1;
 //    printf("Enter two numbers:");
 //	scanf("%d%d", &x, &y);
-	printf("%d; %d", x, y);
+//	printf("%d; %d", x, y);
+//	if (y == 5.5) {
+//	}
 //	int b = a;
 //	b = b + 1;
 //	int c = 7;
