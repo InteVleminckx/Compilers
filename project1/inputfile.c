dd7b20fc4a31fa1a0427e5016ef2e35fe315d822
Maksim Karnaukh <s0201604@ua.ad.ac.be>
2022-05-12 21:34:29 +0200

    a

--- project1/inputfile.c
+++ project1/inputfile.c
@@ -1,24 +1,34 @@
-#include <stdio.h>
+int x = 5;
+// variable redefinition with different type
+float x = 5.0;
+
+int main(){
+    return 0;
+}
+//#include <stdio.h>
 //int f(int b){
 //}
 //int f(int a, int b) {}
-return 5;
-int main() {
+//return 5;
+//void f(int a, int b){}
+//int main() {
+//    f(1);
+//    printf("%d; ", 9++);
+////    printf("%d; ", --11);
+////	int x = 10;
+////	int y = 11;
+////	y = y + 1;
+////	x = x + 1 * y + 1;
+////    printf("Enter two numbers:");
+////    printf("%d; %d", x, y);
+////	scanf("%d%d", &x, &y);
+////	printf("%s; %d", "%s", y);
+////	if (y == 5.5) {
+////	}
+////	int b = a;
+////	b = b + 1;
+////	int c = 7;
+////	c = c + 1;
+//}
 
-    printf("%d; ", 9++);
-    printf("%d; ", --11);
-//	int x = 10;
-//	int y = 11;
-//	y = y + 1;
-//	x = x + 1 * y + 1;
-//    printf("Enter two numbers:");
-//    printf("%d; %d", x, y);
-//	scanf("%d%d", &x, &y);
-//	printf("%s; %d", "%s", y);
-//	if (y == 5.5) {
-//	}
-//	int b = a;
-//	b = b + 1;
-//	int c = 7;
-//	c = c + 1;
-}
+
